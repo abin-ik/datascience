8317ee9befdb5afae5a073e1dbcbf5782bb6eea1
Abin Thomas <abin29192@gmail.com>
2025-09-19 11:40:33 +0530

    numpy

---
+++ sep19a.py
@@ -0,0 +1,13 @@
+import numpy as np
+a=np.array([1,2,3,4,5,6])
+a1=np.array([1.0,2.3,5.1,8.6])
+print(np.sqrt(a))
+print(np.round(a1))
+print(np.floor(a1))
+print(np.pi)
+
+a3=np.array([1,2,3,4,5])
+a4=np.array([6,7,8,9,1])
+print(a3+a4)
+print(a4/a3)
+print(a3**a4)
