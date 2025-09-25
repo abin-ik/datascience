5639ce1e5a2a1553e0245408d9d74fd11739fed3
Abin Thomas <abin29192@gmail.com>
2025-09-25 11:51:13 +0530

    numpy logical functions

--- sep25b.py
+++ sep25b.py
@@ -2,5 +2,5 @@ import numpy as np
 a = np.array([[1,2,3,4],[5,6,7,8]])
 print(a[a>2])
 print(a[(a>2) & (a<8)]) # AND  function
-print(a[(a<5) | (a>7)]) # OR function
+print(a[(a>5) | (a>7)]) # OR function
 print(a[~(a == 2)])  # not function
