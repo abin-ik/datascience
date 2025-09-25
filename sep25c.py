40f8cd6371931adeee0ed34a32e9aa4c250f4cdb
Abin Thomas <abin29192@gmail.com>
2025-09-25 12:05:57 +0530

    Basic Filtering + Sum

--- sep25c.py
+++ sep25c.py
@@ -1,5 +1,5 @@
 # 1. Basic Filtering + Sum
-# Given an array of marks:
+# Given an array of Marks:
 #marks = np.array([45, 78, 23, 90, 67, 88, 34, 59])
 # → Filter only marks greater than 50
 # → Find the sum of these filtered marks
