e1860e77fb357181a5546fbe0e6b537833f98513
Abin Thomas <abin29192@gmail.com>
2025-09-25 11:59:40 +0530

    basic problems

---
+++ sep25d.py
@@ -0,0 +1,12 @@
+# 2. Filtering + Mean
+# Given an array:
+#arr = np.array([12, 45, 67, 89, 34, 22, 90, 100])
+# → Select values between 40 and 90 (exclusive)
+# → Calculate their mean
+
+
+import numpy as np
+a = np.array([12, 45, 67, 89, 34, 22, 90, 100])
+mean = a[(a>40) & (a<90)]
+print(mean)
+print(np.sum(mean))
