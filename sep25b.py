f5b8b0cc0acaeb0dfbfde6129b4f59236b4cd903
Abin Thomas <abin29192@gmail.com>
2025-09-25 11:49:13 +0530

    logical operations

--- sep25b.py
+++ sep25b.py
@@ -1,6 +1,6 @@
 import numpy as np
 a = np.array([[1,2,3,4],[5,6,7,8]])
 print(a[a>2])
-print(a[(a>2) & (a<8)])
-print(a[(a>5) | (a<8)])
-print(~(a == 2))
+print(a[(a>2) & (a<8)]) # AND  function
+print(a[(a<5) | (a>7)]) # OR function
+print(a[~(a == 2)])  # not function
