e6a76a626523c2fd332f9b96e2051060a856ccbe
Abin Thomas <abin29192@gmail.com>
2025-09-26 13:00:07 +0530

    numpy - split

---
+++ sep26q.py
@@ -0,0 +1,8 @@
+# ⿧ split() – Split an array into multiple parts (1D)
+# =======================================
+# Q7: Given: arr = np.array([5, 10, 15, 20, 25, 30])
+# → Split this array into 3 equal parts and print each part
+
+import numpy as np
+a = np.array([5, 10, 15, 20, 25, 30])
+print(np.split(a,3))
