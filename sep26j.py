6b25c227f5ccb973b7ea8f6532372d6f4a67de8b
Abin Thomas <abin29192@gmail.com>
2025-09-26 12:26:11 +0530

    numpy - unique

--- sep26j.py
+++ sep26j.py
@@ -1,3 +1,8 @@
+# =======================================
+# ⿨ unique() – Find unique elements in an array
+# =======================================
+
+
 import numpy as np
 a = np.array([11,2,5,3,2,11,2,3,4,2,3])
 print(np.unique(a))
